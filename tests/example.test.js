2ba42f45de42b0dbf2752ebee94a16a14ec4321f
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-30 23:45:11 +0300

    expect result

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('My first puppeteer test', () => {
     it('should launch the browser', async function() {
         const browser = await puppeteer.launch({ 
-            headless: false, 
+            headless: true, 
             slowMo: 100, 
             devtools: false })
         const page = await browser.newPage()
@@ -11,9 +12,12 @@ describe('My first puppeteer test', () => {
         const title = await page.title()
         const url = await page.url()
         const text = await page.$eval('h1', element => element.textContent)
-        const count = await.page.$$eval('p', element => element.length)
-        console.log('Text tn the H1: ' + text)
-        console.log('Number of P tags in the page: ' + count)
+        const count = await page.$$eval('p', element => element.length)
+
+        expect(title).to.be.a('string', 'Example Domain')
+        expect(url).to.include('example.com')
+        expect(text).to.be.a('string', 'Example Domain')
+        expect(count).to.equal(2)
         await browser.close()
     })
 })
