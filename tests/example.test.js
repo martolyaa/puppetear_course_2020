739ccc7e72579d8b3d7973e1e7ff855a1f9be222
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-30 23:27:00 +0300

    get title and Url

--- tests/example.test.js
+++ tests/example.test.js
@@ -7,16 +7,12 @@ describe('My first puppeteer test', () => {
             slowMo: 100, 
             devtools: false })
         const page = await browser.newPage()
-        await page.goto('http://devexpress.github.io/testcafe/example/')
-        await page.type('#developer-name', 'Mike', { delay: 200})
-        await page.waitFor(2000)
-        await page.click('#tried-test-cafe', { clickCount: 1})
-        await page.select('#preferred-interface', 'Javascript API')
-        const message = 'Lets fill that message with some text'
-        await page.type('#comments', message)
-        await page.click('#submit-button')
-        await page.waitForSelector('.result-content')
-        await page.waitFor(1000)
+        await page.goto('http://example.com/')
+        const title = await page.title()
+        const url = await page.url()
+        
+        const.log('TITLE: ' + title)
+        const.log('URL: ' + url)
         await browser.close()
     })
 })
