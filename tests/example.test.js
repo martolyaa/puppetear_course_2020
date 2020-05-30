ed068f298629ef0dd9dd5fead256abaaccd92760
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-30 23:56:03 +0300

    test for search box

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,13 +4,13 @@ const expect = require('chai').expect
 describe('My first puppeteer test', () => {
     it('should launch the browser', async function() {
         const browser = await puppeteer.launch({ 
-            headless: true, 
+            headless: false, 
             slowMo: 100, 
             devtools: false })
         const page = await browser.newPage()
         await page.setDefaultTimeout(10000)
         await page.setDefaultNavigationTimeout(20000)
-        
+
         await page.goto('http://example.com/')
         const title = await page.title()
         const url = await page.url()
@@ -21,6 +21,13 @@ describe('My first puppeteer test', () => {
         expect(url).to.include('example.com')
         expect(text).to.be.a('string', 'Example Domain')
         expect(count).to.equal(2)
+
+        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.waitForSelector('#searchTerm')
+        await page.type('#searchTerm', 'Hello World')
+        await page.keyboard.press('Enter', {delay: 10})
+        await page.waitFor(5000)
+
         await browser.close()
     })
 })
