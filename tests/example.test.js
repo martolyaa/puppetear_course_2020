53e6cb60ae4f0bda03567cac9249e353e7a67124
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-30 23:03:23 +0300

    Test for input

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,11 +1,15 @@
 const puppeteer = require('puppeteer')
 
 describe('My first puppeteer test', () => {
-    it('should launch the browser', async function(){
-        const browser = await puppeteer.launch({ headless: false})
+    it('should launch the browser', async function() {
+        const browser = await puppeteer.launch({ 
+            headless: false, 
+            slowMo: 100, 
+            devtools: false })
         const page = await browser.newPage()
-        await page.goto('http://example.com/')
-        await page.waitForSelector('h1')
+        await page.goto('http://devexpress.github.io/testcafe/example/')
+        await page.type('#developer-name', 'Mike', { delay: 200})
+        await page.waitFor(5000)
         await browser.close()
     })
 })
