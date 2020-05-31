927007e6c907fdfa378d2bad33eed9cd7d7fcec8
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-31 19:37:51 +0300

    Puppeteer with Custom Command 1

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,5 +1,10 @@
 const puppeteer = require('puppeteer')
 const expect = require('chai').expect
+// const path = require('path')
+
+const { click } = require('../lib/helpers')
+const { getText } = require('../lib/helpers')
+const { getCount } = require('../lib/helpers')
 
 describe('My first puppeteer test', () => {
     let browser
@@ -10,8 +15,7 @@ describe('My first puppeteer test', () => {
             $slowMo: 10, 
             devtools: false
         })
-        const context =await browser.createIncognitoBrowserContext()
-        page = await context.newPage()
+        page = await browser.newPage()
         await page.setDefaultTimeout(10000)
         await page.setDefaultNavigationTimeout(20000)
     })
@@ -33,17 +37,20 @@ describe('My first puppeteer test', () => {
         await page.waitForXPath('//h1')
         const title = await page.title()
         const url = await page.url()
-        const text = await page.$eval('h1', element => element.textContent)
-        const count = await page.$$eval('p', element => element.length)
+        // const text = await page.$eval('h1', element => element.textContent)
+        // const count = await page.$$eval('p', element => element.length)
+        const text = await getText(page, 'h1')
+        const count = await getCount(page, 'p')
 
         expect(title).to.be.a('string', 'Example Domain')
         expect(url).to.include('example.com')
-        expect(text).to.be.a('string', 'Example Domain')
-        expect(count).to.equal(2)
+        // expect(text).to.be.a('string', 'Example Domain')
+        // expect(count).to.equal(2)
 
         await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.waitForSelector('#signin_button')
-        await page.click('#signin_button')
+        // await page.waitForSelector('#signin_button')
+        // await page.click('#signin_button')
+        await click(page, '#signin_button')
         await page.waitFor(() => !document.querySelector('#signin_button'))
         await page.waitForSelector('#signin_button', { 
             hidden: true, 
