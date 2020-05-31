36ffd9fdee346e4edb821934035408fcfc5136ff
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-05-31 14:21:25 +0300

    Test Hooks

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,15 +2,32 @@ const puppeteer = require('puppeteer')
 const expect = require('chai').expect
 
 describe('My first puppeteer test', () => {
-    it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({ 
+    let browser
+    let page
+    before(async function () {
+        browser = await puppeteer.launch({ 
             headless: false, 
-            slowMo: 100, 
-            devtools: false })
-        const page = await browser.newPage()
+            $slowMo: 10, 
+            devtools: false
+        })
+        page = await browser.newPage()
         await page.setDefaultTimeout(10000)
         await page.setDefaultNavigationTimeout(20000)
+    })
 
+    after(async function() {
+        await browser.close()
+    })
+
+    beforeEach(async function() {
+        // Runs before each test step
+    })
+
+    afterEach(async function() {
+        // Runs before each test step 
+    })
+
+    it('should launch the browser', async function() {
         await page.goto('http://example.com/')
         await page.waitForXPath('//h1')
         const title = await page.title()
